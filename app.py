8ae20722d7234007a40dfb15b06d2ecbe88f3ef4
Sudhu2004 <sudhrshan18@gmail.com>
2025-01-01 16:22:19 +0530

    Updated Files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import threading
 
 import jsons
 from flask import Flask, jsonify, request
-# from pyngrok import ngrok
+from pyngrok import ngrok
 
 from cashflow_model import process_cashflow_data
 
@@ -15,6 +15,7 @@ def predict():
         if not json_data:
             return jsonify({"error": "Invalid input, JSON expected"}), 400
         
+        print(json_data)
         results = process_cashflow_data(jsons.dumps(json_data))
         return jsonify(results), 200
     except Exception as e:
@@ -24,7 +25,6 @@ def run_flask():
     app.run(port=5000)
 
 if __name__ == "__main__":
-    # public_url = ngrok.connect(5000)
-    # print(f"ngrok tunnel: {public_url}")
-    # threading.Thread(target=run_flask).start()
-    run_flask()
+    public_url = ngrok.connect(5000)
+    print(f"ngrok tunnel: {public_url}")
+    threading.Thread(target=run_flask).start()
